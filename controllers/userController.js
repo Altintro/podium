6a28cdd888d5c74755776475b5f48c60f3c46bd9
Fernando Frances <fernando95frances@gmail.com>
2018-03-07 15:40:09 +0100

    trying with filters

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,9 @@ exports.get = (req, res, next) => {
   
     if(name) { filter.name = { $regex: '^'+ name, $options: 'i' }}
     if(alias){ filter.alias = { $regex: '^'+alias, $options: 'i' }}
-    filter.pass = 0
+    filter.pass = '0'
+
+    console.log(filter)
   
     User.list(filter,limit,skip,fields,sort,
       (err, users) => {
